1a0ff51dd4758e6a6703b0c4dd6396effe2c2514
unknown <tianxuelin47@gmail.com>
2018-01-22 23:36:14 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/userlh/coolweather/db/Province.java
@@ -0,0 +1,29 @@
+package com.example.userlh.coolweather.db;
+
+/**
+ * Created by userLH on 2018/1/22.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
